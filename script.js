59ec36b01a956f96cec6d27124878eda9621fd9f
Mike Criollo <mkcriollo@gmail.com>
2024-06-19 01:22:57 -0400

    fix bug with variable naming, finish javascript adding active class

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const panels = document.querySelectorAll("panel");
+const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClass();
     panel.classList.add("active");
+    console.log(panel);
   });
 });
 
 function removeActiveClass() {
-  panel.forEach((panel) => {
+  panels.forEach((panel) => {
     panel.classList.remove("active");
   });
 }
